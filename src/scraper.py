0603fd647d6111413ec227aff19dc69e972a8aa0
Ciro Callazans <ccallazans@protonmail.com>
2022-04-05 22:24:15 -0300

    Update

--- src/scraper.py
+++ src/scraper.py
@@ -5,15 +5,16 @@ import pandas
 def access_page(url):
     try:
         request = requests.get(url)
+        return request.content
 
     except BaseException as err:
         raise SystemExit(err)
     
-    return request.content
-
 def parse_page_content(page):
     soup = BeautifulSoup(page, 'html.parser')
-    print(soup.prettify())
+    for div in soup.find_all(class_="DealGridItem-module__dealItem_2X_WLYkJ3-dM0LtXI9THcu DealGridItem-module__withBorders_2jNNLI6U1oDls7Ten3Dttl DealGridItem-module__withoutActionButton_2OI8DAanWNRCagYDL2iIqN"):
+        print(div.prettify())
+
 
 
 
