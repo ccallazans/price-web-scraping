359f39425e0d647bad16352c5fa45768d3c423ae
Ciro Callazans <ccallazans@protonmail.com>
2022-04-11 20:50:42 -0300

    fix save data

--- src/app.py
+++ src/app.py
@@ -1,1 +1,22 @@
-main
+import sys
+import json
+from product import access_page, parse_page_content, append_data
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+HEADERS = ({
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    'Accept-Language': 'en-US, en;q=0.5'
+})
+
+
+def main(link):
+    page = access_page(link, HEADERS)
+    collected_data = parse_page_content(page)
+
+    print("Collected Data: \n", json.dumps(collected_data, indent=4))
+    return append_data(collected_data)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
--- src/product.py
+++ src/product.py
@@ -5,26 +5,32 @@ from bs4 import BeautifulSoup
 
 DATA_FOLDER = 'data/'
 
+
 def access_page(url, header):
     request = requests.get(url, headers=header)
     
     return request.content
 
+
 def get_product_name(page):
     find_name = page.find_all('span', id="productTitle")
+    find_name = find_name[0].contents
     
     if len(find_name) == 0:
         return "Empty Name"
-    
+
     return find_name[0].strip(' ')
+
 
 def get_product_price(page):
     find_price = page.find_all('span', class_="a-price-whole")
+    find_price = find_price[0].contents
     
     if len(find_price) == 0:
         return "Empty Price"
     
     return find_price[0].strip(' ')
+
 
 def get_product_link(page):
     find_link = page.find_all('link', rel="canonical")
@@ -32,7 +38,8 @@ def get_product_link(page):
     if len(find_link) == 0:
         return "Empty Link"
     
-    return find_link[0].strip(' ')
+    return str(find_link[0]).split(' ')[1].split('href=')[1].strip('"')
+
 
 def get_product_image(page):
     find_image = page.find_all('img', id="landingImage")
@@ -43,16 +50,18 @@ def get_product_image(page):
     
     return image.strip('"')
 
+
 def to_dict(*args):
 
     dictionary = {
         "Name": args[0],
         "Price": args[1],
-        "Link": args[3],
-        "Image": args[4]
+        "Link": args[2],
+        "Image": args[3]
     }
 
     return dictionary
+
 
 def parse_page_content(page):
     soup = BeautifulSoup(page, 'html.parser')
@@ -64,11 +73,12 @@ def parse_page_content(page):
     
     return to_dict(product_name, product_price, product_link, product_image)
 
+
 def append_data(json_data):
-    if os.path.exists('data/prices.csv'):
-        data = pd.read_csv('data/prices.csv')
+    if os.path.exists('src/' + DATA_FOLDER + 'prices.csv'):
+        data = pd.read_csv('src/' + DATA_FOLDER + 'prices.csv')
     else:
         data = pd.DataFrame(columns=["Name","Price","Link","Image"])
         
     data = data.append(json_data, ignore_index=True)
-    return data.to_csv('DATA_FOLDER/prices.csv')
+    return data.to_csv('src/' + DATA_FOLDER + 'prices.csv', index=False)
